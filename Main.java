939ce7c93b9992a542e419d7209fca432ce4ad87
Ranjith D <126503146+Ranjithdurai451@users.noreply.github.com>
2025-08-24 13:11:23 +0530

    Add solution: Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+// Write a LeetCode-like problem in a function to analyze and generate tests in comments
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
